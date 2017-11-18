8aee63bc1e51e4a4105654e9c27bc289b1465613
Shail Desai <sh965795@dal.ca>
2017-11-18 16:39:50 -0400

    blinking Flashlight

---
+++ MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.shail.flashlightphase2;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.media.MediaPlayer;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
+    int i =1;
+    int z= 100;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for (i = 1; i <= 100; i++) {
+
+            try {
+                Thread.sleep(4000);//Turn ON
+                flasLight();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+
+                Thread.sleep(8000);//Turn ON
+                flasLoff();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public void flasLight() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            String cameraId = null; // Usually back camera is at 0 position.
+            try {
+                try {
+                    cameraId = camManager.getCameraIdList()[0];
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+
+
+                camManager.setTorchMode(cameraId, true);
+
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+    public void flasLoff() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            String cameraId = null; // Usually back camera is at 0 position.
+            try {
+                try {
+                    cameraId = camManager.getCameraIdList()[0];
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+
+
+                camManager.setTorchMode(cameraId, false);
+                Thread.sleep(8000);//Turn ON//Turn ON
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
